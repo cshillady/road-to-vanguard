6d5ad3d0e534540ff3d89f3f3a0456301a0ed365
cshillady <c.shillady24@gmail.com>
2022-04-18 13:54:04 -0400

    Update 1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,6 @@ public class Main
 {
     public static void main(String[] args)
     {
-        System.out.println("My name is Connor David Shillady.  This is the initial branch.");
+        System.out.println("My name is Connor David Shillady.  This is the initial branch from main.");
     }
 }
